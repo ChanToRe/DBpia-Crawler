f4f24eb5bef275233fa7ee832b11cf60d5a9a91a
chantore <wnrnrxla@gmail.com>
2022-01-25 16:28:46 +0900

    Fix : Next page
    10페이지 이후로 넘어가지 않는 문제 해결

--- DBpia-Crawler.py
+++ DBpia-Crawler.py
@@ -11,111 +11,94 @@ import time
 #하단에 입력해주세요.
 savename = "DBpia-Crawler" #저장 파일명
 keyword = "고고학" #검색어
-target_page = 3 #원하는 페이지
+target_page = 187 #원하는 페이지
 
+#접속 및 세팅
 url = "https://www.dbpia.co.kr/"
 driver = webdriver.Chrome('/usr/local/bin/chromedriver') #Chromedriver 주소
 driver.get(url)
-search_box = driver.find_element_by_xpath('//*[@id="keyword"]')
-search_box_btn = driver.find_element_by_xpath('//*[@id="bnHead"]/div[3]/div/div[1]/div[1]/a')
-search_box.send_keys(keyword)
-search_box_btn.click()
+driver.find_element_by_xpath('//*[@id="keyword"]').send_keys(keyword)
+driver.find_element_by_xpath('//*[@id="bnHead"]/div[3]/div/div[1]/div[1]/a').click()
 
+#저장 리스트
 titleL = []
 authorL = []
 publisherL = []
 journalL = []
 volumeL = []
 dateL = []
-abstractL = []
 
 print("====start====")
 
-iCount = 0
-i = 1
+#크롤링
+for i in range(target_page):
 
-while i <= target_page:
     items_source = driver.page_source
     soup = BeautifulSoup(items_source, 'html.parser')
     items = soup.find('div','searchListArea').find('div','listBody').find('ul').find_all('li', 'item')
+    time.sleep(2)
 
     for item in items :
-        iCount += 1
 
+        #제목
         title = ''
         try : title = item.find('div','titWrap').find('a').text
         except : title = ''
         titleL.append(title)
 
+        #저자
         author = ''
         try : author = item.find('li','author').text
         except : author = ''
         authorL.append(author)
 
+        #발간기관
         publisher = ''
         try : publisher = item.find('li','publisher').text
         except : publisher = ''
         publisherL.append(publisher)
 
+        #발간지
         journal = ''
         try : journal = item.find('li','journal').text
         except : journal = ''
         journalL.append(journal)
 
+        #회차
         volume = ''
         try : volume = item.find('li','volume').text
         except : volume = ''
         volumeL.append(volume)
 
+        #발간일
         date = ''
         try : date = item.find('li','date').text
         except : date = ''
         dateL.append(date)
 
-        abstract = ''
-        pUrl = ''
-        try : pUrl = item.find('div','titWrap').find('a')['href']
-        except : pUrl = ''
-        if (pUrl != ''):
-            pUrl = url + pUrl
-            driver.get(pUrl)
-            try : driver.find_element_by_xpath('//*[@id="#pub_modalOrganPop"]').click()
-            except : pass
-            time.sleep(0.1)
-            try : driver.find_element_by_xpath('//*[@id="#pub_modalLoginPop"]').click()
-            except : pass
-            try :
-                driver.find_element_by_xpath('//*[@id="pub_abstract"]/div[2]/div/div[1]/div[2]/a').click()
-                eachPage = driver.page_source
-                ePsoup = BeautifulSoup(eachPage, 'html.parser')
-                abstract = ePsoup.find('div','abstFull').find('p','article').text
-            except : abstract = ''
-        abstractL.append(abstract)
-
-#Next Page
-    try :
-        driver.get(url)
-        search_box = driver.find_element_by_xpath('//*[@id="keyword"]')
-        search_box_btn = driver.find_element_by_xpath('//*[@id="bnHead"]/div[3]/div/div[1]/div[1]/a')
-        search_box.send_keys(keyword)
-        search_box_btn.click()
+    #다음 페이지 클릭
+    try:
+        driver.find_element_by_xpath(f'//*[@id="pcPaging{str(i+1)}"]').click()
         time.sleep(2)
-        nxt = driver.find_element_by_xpath(f"/html/body/div[3]/div[2]/div[1]/div[2]/div[2]/div[3]/div[3]/a[{i+1}]")
-        nxt.click()
-        print(i, "page Done")
-        i += 1
-        time.sleep(2)
-    except : break
+    except:
+        pass
+    print(i, "Done")
+    i += 1
+    time.sleep(2)
+    
+    #10페이지 마다 Next 버튼 클릭
+    if i % 10 == 0:
+        driver.find_element_by_xpath('//*[@id="next"]').click()
 
 print("====Done!====")
 
+#저장
 resultDict = dict(title = titleL,
                 author = authorL,
                 publisher = publisherL,
                 journal = journalL,
                 volume = volumeL,
-                date = dateL,
-                abstract = abstractL)
+                date = dateL,)
 
 csv_Name = "~/Desktop/{}.csv".format(savename)
 xlsx_Name = "~/Desktop/{}.xlsx".format(savename)
